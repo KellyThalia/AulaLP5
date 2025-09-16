a71a373ae66d0dcdfa0902b9925b3d3273ad4336
KellyThalia <kelly.oliveira5@estudante.edu.br>
2025-09-16 09:14:08 -0400

    Fizemos o mapeamneto do hibernate. Criamos o DAO, e por útimo corrigimos o mapeamento.

---
+++ src/dao/ProdutosDAO.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import bean.Produtos;
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+
+/**
+ *
+ * @author u02624735267
+ */
+public class ProdutosDAO extends AbstractDAO {
+
+    @Override
+    public void insert(Object object) {
+        session.beginTransaction();
+        session.save(object);
+        session.getTransaction().commit();  
+    }
+
+    @Override
+    public void update(Object object) {
+        session.beginTransaction();
+        session.flush();
+        session.clear();
+        session.save(object);
+        session.getTransaction().commit();
+    }
+
+    @Override
+    public void delete(Object object) {
+        session.beginTransaction();
+        session.flush();
+        session.clear();
+        session.delete(object);
+        session.save(object);
+        session.getTransaction().commit();    
+    }
+
+    @Override
+    public Object list(int codigo) {
+        session.beginTransaction();
+        Criteria criteria = session.createCriteria(Produtos.class);
+        criteria.add(Restrictions.eq("idprodutos", codigo) );
+        List lista = criteria.list();
+        session.beginTransaction().commit();
+        return lista;
+    }
+
+    @Override
+    public Object listAll() {
+        session.beginTransaction();
+        Criteria criteria = session.createCriteria(Produtos.class);
+        List lista = criteria.list();
+        session.beginTransaction().commit();
+        
+        return lista;
+    }
+
+    
+}
